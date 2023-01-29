190c0a15812180faeaa840498b63512099967dc4
AbinavRavi <subramathreya@gmail.com>
2023-01-29 18:55:58 +0100

    model changes

--- src/model.rs
+++ src/model.rs
@@ -24,3 +24,15 @@ struct LoginInfo{
     email: String,
     password: String
 }
+
+impl LoginInfo{
+    fn validate_email() -> None{
+        let re = Regex::new(r"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$").unwrap();
+        let email_id = SignupInfo.email;
+        assert!(re.is_match(email_id));
+    }
+
+    fn validate_password() -> io::Result<()>{
+        Ok(())
+    }
+}
